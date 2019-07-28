f66e9e168fdd86cabcc6acce2f42ebea89866a8f
Oblivious <me262thanos@gmail.com>
2019-07-28 14:06:42 +0300

    Added base files.

---
+++ assets/server/main.ts
@@ -0,0 +1,28 @@
+import express = require("express");
+import path = require("path");
+
+const clientsource: string = path.join(__dirname, "/../client/");
+const exportsource: string = path.join(__dirname, "/../export/");
+const serversource: string = path.join(__dirname, "/./");
+
+class Server {
+    app: express.Application;
+    port: number;
+
+    constructor(port: number) {
+        this.port = port;
+        this.app = express();
+    }
+
+    run(): void {
+        this.app.get("/", (req, res) => {
+            res.sendFile(clientsource + "index.html");
+        });
+
+        this.app.listen(this.port);
+        console.log("Listening on port " + this.port);
+    }
+}
+
+const server = new Server(8888);
+server.run();
